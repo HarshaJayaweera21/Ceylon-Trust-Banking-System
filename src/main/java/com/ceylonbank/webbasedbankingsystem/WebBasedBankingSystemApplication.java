40140db3ac9d2fa21681097db890f550b7aa097a
HarshaJayaweera21 <harshajayaweera1965@gmail.com>
2025-08-22 14:30:00 +0530

    Initialize Web Based Banking System with Maven setup and project structure

---
+++ src/main/java/com/ceylonbank/webbasedbankingsystem/WebBasedBankingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ceylonbank.webbasedbankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedBankingSystemApplication.class, args);
+    }
+
+}
+
